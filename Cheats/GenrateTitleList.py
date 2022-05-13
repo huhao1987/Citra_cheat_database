cb462ba4ea0e72062198b9b7b200bfc7f7720a8f
Hao <huhao1987@gmail.com>
2022-05-13 13:48:11 +1000

    Modified the script to genarate list as json

--- Cheats/GenrateTitleList.py
+++ Cheats/GenrateTitleList.py
@@ -1,4 +1,5 @@
 import os
+import json
 import re
 print(os.getcwd())
 
@@ -16,16 +17,24 @@ save_en_path = base_path + '\\cheatlist_en.txt'
 #Chinese output file
 save_zh_path = base_path + '\\cheatlist_zh.txt'
 
-
 def write_cheattitle(baselanpath,savepath):
         fw=open(savepath, "w", encoding="utf-8")
         file_path = os.walk(baselanpath)
+        first = True
+        fw.write('{"cheats":[')
         for root, dirs, files in file_path:
                 if baselanpath+'\\' in root:
                         text=files[0]
-                        print(root.replace(baselanpath+'\\','')+':'+text)
-                        t=root.replace(baselanpath+'\\','')+':'+text+"\n"
-                        fw.write(t)
+                        destitle='"title":"'+root.replace(baselanpath+'\\','')+'"'
+                        cheattitle='"cheat":"'+text+'"'
+                        if first:
+                                first=False
+                                fw.write('{'+destitle+','+cheattitle+'}')
+                        else:
+                                fw.write(',{'+destitle+','+cheattitle+'}')
+
+        fw.write(']}')
+
 
 
 
