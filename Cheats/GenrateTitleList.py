5a0e2b5727d70c3fd08adf0bb8f31d91ec1ca487
Hao <huhao1987@gmail.com>
2022-05-13 12:46:47 +1000

    Update script Update Chinese and English lists

--- Cheats/GenrateTitleList.py
+++ Cheats/GenrateTitleList.py
@@ -2,14 +2,34 @@ import os
 import re
 print(os.getcwd())
 
+#Base folder
 base_path=os.getcwd()
-file_path=os.walk(base_path)
-save_path = base_path+'\\cheatlist.txt'
-fw = open(save_path, "w")
+#English folder
+en_base_path=base_path+'\\En'
+#Chinese folder
+zh_base_path=base_path+'\\Zh'
 
-def write_cheattitle(path):
-        for root, dirs, files in path:
-                if base_path+'\\' in root:
-                        print(root.replace(base_path+'\\','')+':'+files[0])
-                        fw.write(root.replace(base_path+'\\','')+':'+files[0]+"\n")
-write_cheattitle(file_path)
+
+#English output file
+save_en_path = base_path + '\\cheatlist_en.txt'
+
+#Chinese output file
+save_zh_path = base_path + '\\cheatlist_zh.txt'
+
+
+def write_cheattitle(baselanpath,savepath):
+        fw=open(savepath, "w", encoding="utf-8")
+        file_path = os.walk(baselanpath)
+        for root, dirs, files in file_path:
+                if baselanpath+'\\' in root:
+                        text=files[0]
+                        print(root.replace(baselanpath+'\\','')+':'+text)
+                        t=root.replace(baselanpath+'\\','')+':'+text+"\n"
+                        fw.write(t)
+
+
+
+write_cheattitle(en_base_path,save_en_path)
+write_cheattitle(zh_base_path,save_zh_path)
+
+
